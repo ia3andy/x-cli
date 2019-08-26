bbe0ed0925e97af3083b9d5d1ea921bee4355b38
ia3andy <adamevin@redhat.com>
2019-08-26 17:30:47 +0200

    Add name and version

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ func main() {
 		println(err.Error())
 		return
 	}
-
+	app.Name = "xd - This tiny tool wrap all the complex commands you always use in a project."
+	app.Version = "0.0.1"
 	app.Commands = cmds.GenCommands(config)
 
 	err = app.Run(os.Args)
